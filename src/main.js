5b8ec337489bf7bbae42172c9f9fb274d35bfcb4
Radu Memetea <r.memetea@proxima-idea.com>
2022-01-17 15:24:11 +0200

    endpoint change

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 require("./store/subscriber");
 
-axios.defaults.baseURL = "https://dev.bugshot.de/api/v1/";
+axios.defaults.baseURL = "https://dev-api.bugshot.de/api/v1/";
 axios.defaults.headers = {
 	Accept: "application/json",
 	"Content-Type": "application/json",
