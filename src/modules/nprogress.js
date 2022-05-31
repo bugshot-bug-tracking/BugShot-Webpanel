2e2477f26b32ce58b8115a39a2347999b3fb71df
r.memetea <r.memetea@proxima-idea.com>
2022-05-31 11:54:07 +0300

    fix: :bug: show company creator in team table

--- src/modules/nprogress.js
+++ src/modules/nprogress.js
@@ -2,13 +2,10 @@ import NProgress from "nprogress";
 
 export const install = (app, router) => {
 	router.beforeEach((to, from) => {
-		console.log("before");
 		if (to.path !== from.path) NProgress.start();
 	});
 
 	router.afterEach(() => {
-		console.log("after");
-
 		NProgress.done();
 	});
 };
