d441a92208f796e748807652f1abbbd7d933a02b
r.memetea <r.memetea@proxima-idea.com>
2022-06-06 13:43:55 +0300

    chore(package): :heavy_plus_sign: add vue-plugin-layouts and vue-plugin-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 
+import Pages from "vite-plugin-pages";
+import Layouts from "vite-plugin-vue-layouts";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
@@ -23,11 +26,33 @@ export default defineConfig({
 
 		// https://github.com/antfu/unplugin-vue-components
 		Components({
+			dirs: ["src/components", "src/templates"],
 			// allow auto load markdown components under `./src/components/`
 			extensions: ["vue", "md"],
 			// allow auto import and register components used in markdown
 			include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
 			dts: "src/components.d.ts",
 		}),
+
+		Pages({
+			extensions: ["vue"],
+			extendRoute(route, parent) {
+				if (
+					route.name === "all" ||
+					String(route.path).match(/^\/auth/i)
+				) {
+					// Index is unauthenticated.
+					return route;
+				}
+
+				// Augment the route with meta that indicates that the route requires authentication.
+				return {
+					...route,
+					meta: { ...route.meta, requiresAuth: true },
+				};
+			},
+		}),
+
+		Layouts(),
 	],
 });
