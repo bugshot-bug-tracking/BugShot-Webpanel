fadbd030892d0f72fa0685e91476cffd28435006
r.memetea <r.memetea@proxima-idea.com>
2022-06-09 12:12:30 +0300

    refactor: :label: add typescript base support

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+import { App } from "vue";
+import { Router } from "vue-router";
+import { HeadClient } from "@vueuse/head";
+
+interface AppContext<HasRouter extends boolean = true> {
+	app: App<Element>;
+	router: HasRouter extends true ? Router : undefined;
+	head: HeadClient | undefined;
+}
+
+export type UserModule = (ctx: AppContext) => void;
