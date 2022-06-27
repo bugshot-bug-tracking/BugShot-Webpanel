76a961f866ad6e5d7f319738ff9ae24e9ad43532
r.memetea <r.memetea@proxima-idea.com>
2022-06-27 14:46:04 +0300

    refactor: :recycle: generalize the project card and group container

--- src/components/GroupContainer.vue
+++ src/components/GroupContainer.vue
@@ -2,8 +2,12 @@
 <template>
 	<div class="group-container">
 		<div class="top">
-			<div class="text-left">{{ mainText }}</div>
-			<div class="text-right">{{ secondText }}</div>
+			<div class="text-left">
+				<slot name="top-left"> [PH] Group </slot>
+			</div>
+			<div class="text-right">
+				<slot name="top-right"> </slot>
+			</div>
 		</div>
 
 		<div class="items">
@@ -12,54 +16,38 @@
 	</div>
 </template>
 
-<script setup>
-const props = defineProps({
-	mainText: {
-		required: true,
-		type: String,
-		default: "Group",
-	},
-
-	secondText: {
-		type: String,
-		default: "",
-	},
-});
-</script>
-
 <style lang="scss" scoped>
 .group-container {
 	display: flex;
 	flex-direction: column;
-	font-size: 18px;
-	padding: 20px 40px;
+	padding: 1rem 2rem;
 
 	> .top {
 		display: flex;
 		justify-content: space-between;
-		width: 350px;
+		width: 22rem;
 		border-bottom: 1px solid hsl(264, 78%, 77%);
-		padding: 0 10px 10px 10px;
+		padding: 0.5rem;
+		padding-top: 0;
 		align-items: center;
 
 		> .text-left {
-			color: black;
-			font-size: 20px;
+			font-size: 1.25rem;
 			font-weight: 600;
 			text-align: left;
 		}
 
 		> .text-right {
+			font-size: 1.125rem;
 			color: hsl(265, 79%, 54%);
-			min-width: 120px;
+			min-width: 7.5rem;
 		}
 	}
 
 	> .items {
 		display: flex;
-		flex-direction: row;
 		flex-wrap: wrap;
-		padding: 10px;
+		padding: 0.5rem;
 	}
 }
 </style>
