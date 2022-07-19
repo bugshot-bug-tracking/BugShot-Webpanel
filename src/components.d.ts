ce97d9f324c19b888c29cc0a7ee0dfe072a54f6f
r.memetea <r.memetea@proxima-idea.com>
2022-07-19 15:17:39 +0300

    chore: package upgrade

--- src/components.d.ts
+++ src/components.d.ts
@@ -3,6 +3,8 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
+export {}
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AddBug: typeof import('./components/AddBug.vue')['default']
@@ -51,5 +53,3 @@ declare module '@vue/runtime-core' {
     UserMenu: typeof import('./components/PopupButton/UserMenu.vue')['default']
   }
 }
-
-export {}
