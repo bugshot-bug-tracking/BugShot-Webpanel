cc3209be937f738de57bffce88a4c54218183e29
r.memetea <r.memetea@proxima-idea.com>
2022-11-10 19:13:32 +0200

    chore: :truck: move organisation main component 1 level up
    move the previous main organisation page 1 level up so that in case of noting selected there can be a custom page in the 3rd zone (for now it's only a redirect to the first organization in MainStore)

---
+++ src/pages/organization.vue
@@ -0,0 +1,58 @@
+<template>
+	<main class="main-loading" v-if="loading">
+		<img src="/src/assets/animations/loading.svg" alt="loading circle" />
+	</main>
+
+	<main class="main-grid" v-else-if="organizations.length > 0">
+		<section class="grid-sidebar">
+			<OrganizationNavbar />
+		</section>
+
+		<section class="grid-page">
+			<RouterView />
+		</section>
+	</main>
+
+	<main class="main-empty" v-else>
+		<section>
+			<img src="/src/assets/images/nothing_to_show.svg" alt="empty boxes" w-88 h-88 />
+
+			<h1 w-88 text-6>{{ $t("no_organizations") }}</h1>
+
+			<p>{{ $t("please_add_new_one") }}</p>
+
+			<CreateOrganizationModal :primary_button="true" />
+		</section>
+	</main>
+</template>
+
+<script setup lang="ts">
+import CreateOrganizationModal from "./organization/components/CreateOrganizationModal.vue";
+import OrganizationNavbar from "./organization/components/OrganizationNavbar.vue";
+import { useMainStore } from "~/stores/main";
+
+const loading = ref(true);
+
+const store = useMainStore();
+
+const organizations = computed(() => store.getOrganizations);
+
+onMounted(async () => {
+	// fetch organizations data and set the appropriate main based on store.organizations
+	// TODO add an error main component
+	try {
+		await store.initOrganizations();
+	} catch (error) {
+		console.log(error);
+	} finally {
+		loading.value = false;
+	}
+});
+</script>
+
+<style lang="scss" scoped></style>
+
+<route lang="yaml">
+meta:
+    layout: default2
+</route>
