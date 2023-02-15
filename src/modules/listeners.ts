07d07d0834abc529a2d3f8e6599fa235c8b9f0f0
r.memetea <r.memetea@proxima-idea.com>
2023-02-15 18:33:36 +0200

    @35d7952cc0f0dacd1882f71b3f01651dae5660f3

--- src/modules/listeners.ts
+++ src/modules/listeners.ts
@@ -1,7 +1,6 @@
 import { UserModule } from "~/types";
-import { pusher, echo } from "~/composables/listeners";
+import { echo } from "~/composables/listeners";
 
 export const install: UserModule = ({ app, router }) => {
-	app.provide("pusher", pusher);
 	app.provide("echo", echo);
 };
