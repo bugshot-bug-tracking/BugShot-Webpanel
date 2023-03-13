d71e94ab01421f6e030cc7dcbcbccc9487bba2f4
r.memetea <r.memetea@proxima-idea.com>
2023-03-13 12:11:33 +0200

    refactor: update new user check

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -18,7 +18,8 @@ const redirect = () => {
 		if (
 			authStore.new_user === true &&
 			authStore.user.attributes.trial_end_date == null &&
-			(authStore.user.attributes.subscriptions?.length ?? 0) === 0
+			(authStore.user.attributes.subscriptions?.length ?? 0) === 0 &&
+			(store.organizations?.length ?? 0) <= 1
 		) {
 			return route.replace({
 				name: "new",
