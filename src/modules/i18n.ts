049334182cee7aae4d7dd0389d96621c34485af2
r.memetea <r.memetea@proxima-idea.com>
2023-04-11 13:54:50 +0300

    refactor: :recycle: refactor i18n install to support global use (ex. pinia)

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -3,32 +3,17 @@ import datetimeFormats from "~/../locales/datetimeFormats";
 import { UserModule } from "~/types";
 import messages from "@intlify/unplugin-vue-i18n/messages";
 
-/**
- *  Import i18n resources
- *  https://vitejs.dev/guide/features.html#glob-import
- *
- *  Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
- *  manual method of importing raw filles and compiling at runtime
- */
-
-// const messages = Object.fromEntries(
-// 	Object.entries(import.meta.glob("/locales/*.json", { eager: true })).map(([key, value]) => {
-// 		const json = key.endsWith(".json");
-// 		return [key.slice(9, json ? -5 : -4), (value as any).default];
-// 	})
-// );
-
-/// pre-compiled messages from vite
+const instance = createI18n({
+	legacy: false,
+	globalInjection: true,
+	locale: import.meta.env.VITE_I18N_LOCALE || "en",
+	fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || "en",
+	messages: messages,
+	datetimeFormats: datetimeFormats,
+});
 
 export const install: UserModule = ({ app }) => {
-	const i18n = createI18n({
-		legacy: false,
-		globalInjection: true,
-		locale: import.meta.env.VITE_I18N_LOCALE || "en",
-		fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || "en",
-		messages: messages,
-		datetimeFormats: datetimeFormats,
-	});
-
-	app.use(i18n);
+	app.use(instance);
 };
+
+export const globalI18nInstance = instance.global;
