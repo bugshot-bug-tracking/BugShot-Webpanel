baaf6fef2ee9c5b97f599bef55132c7ea28668b8
r.memetea <r.memetea@proxima-idea.com>
2023-05-24 16:54:16 +0300

    chore: :technologist:
    move special users to env

--- src/stores/flags.ts
+++ src/stores/flags.ts
@@ -5,19 +5,21 @@ export const useFlagsStore = defineStore("flags", {
 	state: () => ({
 		user: useAuthStore().user,
 
-		users: [1, 2, 6, 22, 96, 4, 114, 9],
+		users: import.meta.env.VITE_SPECIAL_USERS?.split(", ").map((x: string) => Number(x)) as
+			| number[]
+			| undefined,
 	}),
 
 	actions: {},
 
 	getters: {
 		canSeeEverything: (state) => {
-			if (state.user?.id) if (state.users.some((u) => u === state.user?.id)) return true;
+			if (state.user?.id) if (state.users?.some((u) => u === state.user?.id)) return true;
 			return false;
 		},
 
 		canEditCompanyTerm: (state) => {
-			if (state.user?.id) if (state.users.some((u) => u === state.user?.id)) return true;
+			if (state.user?.id) if (state.users?.some((u) => u === state.user?.id)) return true;
 			return false;
 		},
 	},
