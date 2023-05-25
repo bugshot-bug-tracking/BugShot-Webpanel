c0144ebb352240bd2c3d1c72427ebf99b09957fd
r.memetea <r.memetea@proxima-idea.com>
2023-05-25 11:55:46 +0300

    feat: :building_construction: add logout page

---
+++ src/pages/auth/logout.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup lang="ts">
+const router = useRouter();
+onMounted(() => {
+	router.push({ name: "login" });
+});
+</script>
+
+<route lang="yaml">
+name: logout
+</route>
